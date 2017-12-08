ea155a9ad5751e10b010abcb7fcc4554a2ea2b08
clover89 <smarig16@ru.is>
2017-12-08 17:54:54 +0000

    Changed provisioning logic. Should now update only of instance is running, else provision new one.

--- provisioning/provision-new-environment.sh
+++ provisioning/provision-new-environment.sh
@@ -6,8 +6,15 @@ then
     export GIT_COMMIT=$(git rev-parse HEAD)
 fi
 
-
-source ./create-aws-docker-host-instance.sh
-source ./update-env.sh ${INSTANCE_PUBLIC_NAME}
-
-echo New environment provisioned
+# If dir exists, instance is running. Updatde only.
+if [ -d "ec2_instance" ]; then
+  echo Instance already running, updating...
+  INSTANCE_PUBLIC_NAME_=$(cat ./ec2_instance/instance-public-name.txt)
+  source ./update-env.sh ${INSTANCE_PUBLIC_NAME_}
+  echo Environment updated
+else
+  echo No instance detected, provisioning...
+  source ./create-aws-docker-host-instance.sh
+  source ./update-env.sh ${INSTANCE_PUBLIC_NAME}
+  echo New environment provisioned
+fi
