40d9b7a854113c58a1a1b7b96160a2fbadb79080
clover89 <smarig16@ru.is>
2017-12-08 18:43:48 +0000

    That didn't work. Trying again.

--- provisioning/provision-new-environment.sh
+++ provisioning/provision-new-environment.sh
@@ -7,14 +7,14 @@ then
 fi
 
 # If dir exists, instance is running. Updatde only.
-if [ -d "ec2_instance" ]; then
-  echo Instance already running, updating...
-  INSTANCE_PUBLIC_NAME_=$(cat ./ec2_instance/instance-public-name.txt)
-  source ./update-env.sh ${INSTANCE_PUBLIC_NAME_}
-  echo Environment updated
-else
+if [ ! -d ~/ec2_instance/ ]; then
   echo No instance detected, provisioning...
   source ./create-aws-docker-host-instance.sh
   source ./update-env.sh ${INSTANCE_PUBLIC_NAME}
   echo New environment provisioned
+else
+  echo Instance already running, updating...
+  INSTANCE_PUBLIC_NAME_=$(cat ./ec2_instance/instance-public-name.txt)
+  source ./update-env.sh ${INSTANCE_PUBLIC_NAME_}
+  echo Environment updated
 fi
