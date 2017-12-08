82db99ec48edbdc763d131822decb61f0f2548b9
clover89 <smarig16@ru.is>
2017-12-08 19:11:53 +0000

    And more changes... Hoping this will work now.

--- provisioning/provision-new-environment.sh
+++ provisioning/provision-new-environment.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-
 if [ -z "$GIT_COMMIT" ];
 then
     export GIT_COMMIT=$(git rev-parse HEAD)
 fi
 
 # If dir exists, instance is running. Updatde only.
+#if [ ! -d ~/ec2_instance/ ]; then
+#  echo No instance detected, provisioning...
+#  source ./create-aws-docker-host-instance.sh
+#  source ./update-env.sh ${INSTANCE_PUBLIC_NAME}
+#  echo New environment provisioned
+#else
+#  echo Instance already running, updating...
+#  INSTANCE_PUBLIC_NAME_=$(cat ./ec2_instance/instance-public-name.txt)
+#  source ./update-env.sh ${INSTANCE_PUBLIC_NAME_}
+#  echo Environment updated
+#fi
+
 if [ ! -d ~/ec2_instance/ ]; then
-  echo No instance detected, provisioning...
-  source ./create-aws-docker-host-instance.sh
-  source ./update-env.sh ${INSTANCE_PUBLIC_NAME}
-  echo New environment provisioned
-else
-  echo Instance already running, updating...
-  INSTANCE_PUBLIC_NAME_=$(cat ./ec2_instance/instance-public-name.txt)
-  source ./update-env.sh ${INSTANCE_PUBLIC_NAME_}
-  echo Environment updated
+    source ./create-aws-docker-host-instance.sh
 fi
+
+INSTANCE_PUBLIC_NAME=$(cat ~/ec2_instance/instance-public-name.txt)
+source ./update-env.sh ${INSTANCE_PUBLIC_NAME}
